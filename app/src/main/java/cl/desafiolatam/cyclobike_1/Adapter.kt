a73d4aff7f40f62073e29a77ab4207d5cde70584
AlejandroPerezGarcia <alejandro8001@gmail.com>
2020-08-13 14:58:15 -0400

    Hito 4: crear la clase que extiende el viewHolder

---
+++ app/src/main/java/cl/desafiolatam/cyclobike_1/Adapter.kt
@@ -0,0 +1,8 @@
+package cl.desafiolatam.cyclobike_1
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CyclobikeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    
+}
