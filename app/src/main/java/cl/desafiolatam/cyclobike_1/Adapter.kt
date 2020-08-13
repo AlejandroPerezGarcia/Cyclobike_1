0b4b8cadab1482a3029c7dc3dddd2c6c576f0fc1
AlejandroPerezGarcia <alejandro8001@gmail.com>
2020-08-13 15:08:16 -0400

    Hito 5: Crear el adapter (usando el viewholder)
    Hito 5.1: Implementar onCreateViewHolder

--- app/src/main/java/cl/desafiolatam/cyclobike_1/Adapter.kt
+++ app/src/main/java/cl/desafiolatam/cyclobike_1/Adapter.kt
@@ -1,8 +1,28 @@
 package cl.desafiolatam.cyclobike_1
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class CyclobikeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    
+class CyclobikeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+class CycloAdapter :RecyclerView.Adapter<CyclobikeViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CyclobikeViewHolder {
+
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+        return CyclobikeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CyclobikeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
